07aeb9a2a0ac8fd434571d0b62f8214ea7e4ed6d
FelipePortilla <128866686+FelipePortilla@users.noreply.github.com>
2023-08-29 21:37:32 -0500

    fix: :recycle:

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
-const contenedor = document.querySelector('#contenedor');
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '67bc891487msh83d7f6087e5baa4p137206jsn95c6efd2fc62',
+        'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+    }
+};
 
-document.querySelector('#boton-menu').addEventListener('click', () => {
-	contenedor.classList.toggle('active');
-});
+let menuIcon = document.querySelector(".menu-icon");
+let sidebar = document.querySelector(".sidebar");
+let container = document.querySelector(".main-container");
 
-const comprobarAncho = () => {
-	if(window.innerWidth <= 768){
-		contenedor.classList.remove('active');
-	} else {
-		contenedor.classList.add('active');
-	}
-}
+menuIcon.addEventListener('click', () => {
+    sidebar.classList.toggle('small-sidebar')
+    container.classList.toggle('large-container')
+})
 
-comprobarAncho();
 
-window.addEventListener('resize', () => {
-	comprobarAncho();
-});
+fetch("json/jsonVideos.json", options)
+    .then(res => res.json())
+    .then(response => {
+        let selecion = document.querySelector("#myVideos");
+        selecion.insertAdjacentHTML("beforeend", /*html*/`
+            ${response.contents.map((video) => /*html*/`
+                <div class="list-video">
+                    <div class="thumbnails">
+                    <a href="index-playvideo.html?videoId=${video.video.videoId}"><img src="${video.video.thumbnails[video.video.thumbnails.length - 1].url}" alt="videos" class="img-miniatura"/></a>
+                    </div>
+                    <div class="video-info">
+                        <a href="index-playvideo.html">${video.video.title}</a>
+                        <p>${video.video.stats.views} Views &bull; ${video.video.publishedTimeText}</p>
+                    </div>
+                </div>
+            `).join(" ")}
+        `);
+    })
+    .catch(err => console.log(err));
